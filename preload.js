7c6ce4f67e002d7d907e412563ed97ff6b57f6b2
manaphy <manaphy@umich.edu>
2022-03-24 18:37:05 -0400

    Fixes 14x2

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ async function load() {
     await languagePluginLoader.then(() => { console.log('Ready') });
     await pyodide.loadPackage(["numpy", "scipy"]);
     document.getElementById("submit").disabled = false;
+    document.getElementById("submit").innerHTML = "Submit"
 }
 load();
