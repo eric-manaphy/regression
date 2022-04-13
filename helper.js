a360ef60a2ce9166efe028c88afff6367c14fce0
manaphy <manaphy@umich.edu>
2022-04-12 20:30:55 -0400

    Added original data points

--- helper.js
+++ helper.js
@@ -11,16 +11,17 @@ function generateInputArrays(type, arr) {
 
 function generatePlotCode(type, arr, xvar) {
     const params = input_params[type].slice(0, -1);
-    const fixedarr = params.reduce(
+    const fixedarr = input_params[type].reduce(
         (acc, curr, i) => ({...acc, [curr]: [...new Set(arr[i])]}),
         {}
     );
+    const coordinates = arr[0].map((c, i) => arr.map(r => r[i]));
     const fixedparams = params.filter((x) => x !== xvar);
 
     let s = `
 fig, ax = plt.subplots()
 ${xvar} = np.array([${fixedarr[xvar].join(',')}])
-${generatePlotCodeSub(type, fixedarr, xvar, params, fixedparams, '')}
+${generatePlotCodeSub(type, fixedarr, xvar, params, fixedparams, coordinates, [], '')}
 
 buf = io.BytesIO()
 fig.savefig(buf, format='png')
@@ -29,13 +30,20 @@ img_src = 'data:image/png;base64,' + base64.b64encode(buf.getvalue()).decode('UT
     return s;
 }
 
-function generatePlotCodeSub(type, arr, xvar, origparams, fixparams, s) {
-    if(fixparams.length === 0)
-        return `ax.plot(np.reciprocal(${xvar}), np.reciprocal(${type}((${origparams.join(',')}), *popt)))\n`;
+function generatePlotCodeSub(type, arr, xvar, origparams, fixparams, coordinates, currvals, s) {
+    if(fixparams.length === 0) {
+        const yvalues = coordinates
+            .filter((x, i) => JSON.stringify(currvals) === JSON.stringify(x.slice(1, -1)))
+            .map((x) => x.at(-1));
+        return `ax.plot(np.reciprocal(${xvar}), np.reciprocal([${yvalues.join(',')}]), 'o')\n` +
+            `ax.plot(np.reciprocal(${xvar}), np.reciprocal(${type}((${origparams.join(',')}), *popt)))\n`;
+    }
     const currparam = fixparams[0];
     for(let i = 0; i < arr[currparam].length; ++i) {
         s += `${currparam} = np.array([${(arr[currparam][i].toString() + ',').repeat(arr[xvar].length).slice(0, -1)}])\n`;
-        s += generatePlotCodeSub(type, arr, xvar, origparams, fixparams.slice(1), s);
+        currvals.push(arr[currparam][i]);
+        s += generatePlotCodeSub(type, arr, xvar, origparams, fixparams.slice(1), coordinates, currvals, s);
+        currvals = [];
     }
     return s;
 }
